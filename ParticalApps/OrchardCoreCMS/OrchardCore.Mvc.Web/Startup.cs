b19aeb585670f08da40989de523365081a36266a
Дмитрий Горбовский <dimon007@inbox.ru>
2021-10-29 08:52:11 +0300

    Save

---
+++ ParticalApps/OrchardCoreCMS/OrchardCore.Mvc.Web/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace OrchardCore.Mvc.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddOrchardCore()
+                .AddMvc()
+                ;            
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseOrchardCore();
+
+        }
+    }
+}
